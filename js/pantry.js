7280dd447d38ee4bb466726f7f4cf75c3e68bf2a
binghamtm <binghamtm1@gmail.com>
2026-06-13 20:20:11 -0600

    added functionality to see pantry on pantry.html page. also added functionality to clear input for new items

--- js/pantry.js
+++ js/pantry.js
@@ -1,3 +1,5 @@
-import { loadHeaderFooter } from "./utils.mjs";
+import { displayPantry, loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
+const pantryContentsContainer = document.getElementById("pantry-contents");
+displayPantry(pantryContentsContainer);
